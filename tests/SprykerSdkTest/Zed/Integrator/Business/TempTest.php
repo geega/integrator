63723a1725b44532c87e3d0211199df143d5bff9
Svyatoslav Varpikhovsky <varpihovsky@gmail.com>
2021-09-01 13:46:56 +0300

    Updated test file

--- tests/SprykerSdkTest/Zed/Integrator/Business/TempTest.php
+++ tests/SprykerSdkTest/Zed/Integrator/Business/TempTest.php
@@ -1,7 +1,11 @@
 <?php
 
-namespace SprykerSdkTest\Zed\Integrator\Business;
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
 
+namespace SprykerSdkTest\Zed\Integrator\Business;
 
 use SprykerSdkTest\Zed\Integrator\BaseTestCase;
 
